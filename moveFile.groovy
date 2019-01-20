51e7464add97789481475a2d2a6ecc71419cb0ae
nivmichael <nivmichael@gmail.com>
2019-01-20 21:18:25 +0200

    qq

---
+++ moveFile.groovy
@@ -0,0 +1,13 @@
+pipeline {
+	agent any
+	stages {
+	    stage('createFile') {
+	        steps {
+	              bat """
+	                cd c:/users/nivmi/PycharmProjects/github-test
+                    move myapp.py c:/users/nivmi/desktop/tmp
+	              """
+	        }
+	    }
+	}
+}
